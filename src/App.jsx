8448320470f8ef2484e161779971834f8266c8e2
B.L.A.C.K.O.P.S <blueyedevelopers@gmail.com>
2026-03-26 16:18:41 +0300

    Fix case-sensitive import of Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import Layout from './components/Layout';
+import Layout from './components/layout';
 import Dashboard from './pages/Dashboard';
 import Bots from './pages/Bots';
 import TradeHistory from './pages/TradeHistory';
